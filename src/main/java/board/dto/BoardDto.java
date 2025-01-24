0a577dd9b3121f4f6e97a4a0cae494bfda18176d
binys21 <binys1121@ewhain.net>
2025-01-24 23:30:34 +0900

    init:초기 설정 및 데이터 연결

---
+++ src/main/java/board/dto/BoardDto.java
@@ -0,0 +1,12 @@
+package board.dto;
+
+public class BoardDto {
+    private int boardId;
+    private String title;
+    private String contents;
+    private int HitCnt;
+    private  String createDt;
+    private String createdId;
+    private String updatorDt;
+    private String updatorId;
+}
