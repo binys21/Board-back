ae57bbfab9c2802db9d32e884a3ba6587e4a6048
binys21 <binys1121@ewhain.net>
2025-02-04 14:36:39 +0900

    feat: REST URI로 변경

--- src/main/java/board/BoardApplication.java
+++ src/main/java/board/BoardApplication.java
@@ -2,6 +2,8 @@ package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 
 @SpringBootApplication
@@ -9,7 +11,13 @@ public class BoardApplication {
     
 
     public static void main(String[] args) {
+
         SpringApplication.run(BoardApplication.class, args);
+    }
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 
 }
+
